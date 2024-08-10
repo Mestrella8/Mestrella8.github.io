45cc9f1bb5418cbfc6d030ad023165cb4a5c11be
Meredith Estrella Hernandez <mestrella@Merediths-MacBook-Pro.local>
2024-08-10 19:50:13 -0400

    Updating Pages

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function openNav() {
     document.getElementById("mySidenav").style.left = "0";
 }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.left = "-250px";
+function toggleNav() {
+    const sidenav = document.getElementById("mySidenav");
+    const currentLeft = sidenav.style.left;
+
+    if (currentLeft === "0px") {
+        sidenav.style.left = "-250px"; // Close the side navigation
+    } else {
+        sidenav.style.left = "0"; // Open the side navigation
+    }
 }
+
